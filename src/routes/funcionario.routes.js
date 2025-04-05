fa99a490816a8af52f790d85d23a11b53ef587a3
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2025-04-04 22:13:59 -0300

    files removed

--- src/routes/funcionario.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const funcionarioController = require('../controllers/funcionario.controller');
-const { validateFuncionario } = require('../middlewares/validation.middleware');
-
-router.post('/', validateFuncionario, funcionarioController.cadastrar);
-router.get('/', funcionarioController.listar);
-
-module.exports = router;
